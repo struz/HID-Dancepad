f330161629622bbbd80a60612b53092809533dc5
= <gbbwhittington@gmail.com>
2020-04-20 02:20:44 +1000

    Rework things, add serial console command to enable/disable debugging, fix some bugs with panel.h

--- Dancepad/Dancepad.ino
+++ Dancepad/Dancepad.ino
@@ -4,40 +4,126 @@
 //Panel::Panel(int pin, int pressPressure, int releaseDelta, char scanCode)
 Panel panels[4]
 {
-  Panel(A0, 275-25, 20, 'a'), //left
-  Panel(A1, 320-25, 40, 's'), //down
-  Panel(A2, 200-25, 15, 'w'), //up
-  Panel(A3, 275-25, 16, 'd')  //right
+  Panel(A2, 300, 20, '4'),
+  Panel(A1, 370, 20, '2'),
+  Panel(A0, 180, 20, '8'),
+  Panel(A3, 330, 20, '6')
 };
+
+// Debug level - 0 = off, 1 = tap/release info, 2 = raw sensor voltage
+byte debugLevel = 1;
+// Keyboard input enabled/disabled - disable for testing
+bool keyboardEnabled = false;
+
+// Used for debugging
+bool held = false;
+unsigned long millisActivated = 0;
+unsigned long releasedMillis = 0;
+unsigned int pressureThreshold = 260;
+// Report debugLevel=2 results every X milliseconds
+unsigned int debugReportThresholdMillis = 50;
+unsigned long lastReportedDebugMillis;
+
+void handleInput() {
+  if (Serial.available()){
+    String input = Serial.readString();
+    input.trim();
+    Serial.print("Command received: " );
+    Serial.println(input);
+
+    // Command handling
+    // debug <level>
+    if (input.length() == 7 && input.startsWith("debug")) {
+      int tmpDebugLevel = input.charAt(6) - 48;
+      switch (tmpDebugLevel) {
+        case 0:
+        case 1:
+        case 2:
+          debugLevel = tmpDebugLevel;
+          Serial.print("debug level set to ");
+          Serial.println(debugLevel, DEC);
+          break;
+        default:
+          Serial.println("Invalid debug level supplied");
+      }
+      // TODO: keyboard on/off, remember to release all keys that are curerntly held when turning off!!
+    } else {
+      Serial.println("Unknown command");
+    }
+  }
+}
+
+void doKeyPress(char key) {
+  if (keyboardEnabled) {
+    Keyboard.press(key);
+  }
+}
+
+void doKeyRelease(char key) {
+  if (keyboardEnabled) {
+    Keyboard.release(key);
+  }
+}
 
 void setup(void)
 {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (! Serial); // Wait untilSerial is ready - Leonardo
   Keyboard.begin();
+  Serial.println("Hello world");
 }
 
 void loop(void)
 {
-  for (int i = 0; i < 4; i++)
-  {
-    //panel reference
+  // In order of LDUR, like the game
+  int rawPanelVoltages[4];
+  for (int i = 0; i < 4; i++) {
     Panel &panel = panels[i];
-    //prnt(i, analogRead(panel.pin), 0);
     unsigned long mtime = millis();
-    if ((mtime - panel.timeSincePress) > 20)
-    {
-      int pressure = analogRead(panel.pin);
-      if (!panel.pressed && pressure > panel.pressPressure)
-      {
+    int pressure = analogRead(panel.pin);
+    rawPanelVoltages[i] = pressure;
+
+    // timeSincePress is simple debouncing
+    if ((mtime - panel.timeSincePress) > 20) {
+      if (!panel.pressed && pressure < panel.pressPressure) {
+        if (debugLevel >= 1) {
+          Serial.print("[");
+          Serial.print(pressure, DEC);
+          Serial.print("] ");
+          Serial.print(panel.scanCode);
+          Serial.println(" pressed");
+        }
         panel.timeSincePress = mtime;
         panel.pressed = true;
-        Keyboard.press(panel.scanCode);
-      }
-      else if (panel.pressed && pressure < panel.releasePressure)
-      {
+        doKeyPress(panel.scanCode);
+      } else if (panel.pressed && pressure > panel.releasePressure) {
+        if (debugLevel >= 1) {
+          Serial.print("[");
+          Serial.print(pressure, DEC);
+          Serial.print("] ");
+          Serial.print(panel.scanCode);
+          Serial.print(" released, held: ");
+          Serial.print(mtime - panel.timeSincePress, DEC);
+          Serial.print("\n");
+        }
         panel.timeSincePress = mtime;
         panel.pressed = false;
-        Keyboard.release(panel.scanCode);
+        doKeyRelease(panel.scanCode);
       }
     }
   }
+
+  if (debugLevel >= 2) {
+    if ((millis() - lastReportedDebugMillis) > debugReportThresholdMillis) {
+      for (int i = 0; i < 4; i++) {
+        Serial.print(rawPanelVoltages[i]);
+        Serial.print(" ");
+      }
+      Serial.print("\n");
+      lastReportedDebugMillis = millis();
+    }
+  }
+  // Process serial commands
+  handleInput();
 }
--- Dancepad/panel.h
+++ Dancepad/panel.h
@@ -6,12 +6,16 @@ struct Panel
     int pressPressure;
     int releasePressure;
     char scanCode;
+    unsigned long timeSincePress;
+    bool pressed;
   public:
     Panel(int pin, int pressPressure, int releaseDelta, char scanCode)
     {
       this->pin = pin;
       this->pressPressure = pressPressure;
-      this->releasePressure = pressPressure - releaseDelta;
+      this->releasePressure = pressPressure + releaseDelta;
       this->scanCode = scanCode;
+      this->timeSincePress = 0;
+      this->pressed = false;
     }
 };
